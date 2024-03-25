3a479a5879b6a78c0b72b2a7dd042b61c49a7402
mahmudmhb1 <mahmud.mhb1@gmail.com>
2024-03-26 00:19:47 +0600

    bkroy page update

--- src/Components/Bikroy/Bikroy.jsx
+++ src/Components/Bikroy/Bikroy.jsx
@@ -1,28 +1,15 @@
-// import React, { useRef, useState } from "react";
-// Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
 import { BsWechat } from "react-icons/bs";
 import { AiFillSafetyCertificate } from "react-icons/ai";
 import { GoReport } from "react-icons/go";
-
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/navigation";
-import "swiper/css/thumbs";
-
-// import required modules
 import { FaRegArrowAltCircleUp, FaRegStar } from "react-icons/fa";
 import { MdOutlinePhone } from "react-icons/md";
 import { Link } from "react-router-dom";
 import { CiShare2 } from "react-icons/ci";
 const Bikroy = () => {
-  //   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-
   return (
     <div className="bg-[#e7edee]">
       <div className="w-5/6 mx-auto bg-[#ffffff] p-5">
-        <div className="flex justify-between ">
+        <div className="md:flex justify-between ">
           <div className="space-y-3 my-2">
             <h1 className="text-2xl font-bold ">Tecno Spark 6 4/128 (Used)</h1>
             <p className="text-[#707676]">
@@ -30,100 +17,20 @@ const Bikroy = () => {
             </p>
           </div>
           <div className="flex gap-5">
-            <div className="flex gap-3 items-center text-[#707676]">
+            <button className="flex gap-3 items-center text-[#707676]">
+              {" "}
               <CiShare2 /> Share
-            </div>
-            <div className="flex gap-3 items-center text-[#707676]">
+            </button>
+
+            <button className="flex gap-3 items-center text-[#707676]">
+              {" "}
               <FaRegStar /> Save Ad
-            </div>
+            </button>
           </div>
         </div>
-        <div className="flex gap-4 ">
+        <div className="md:flex gap-4 ">
           <div>
-            <div className="w-[552px] h-[620px] border">
-              {/* <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-        }}
-        spaceBetween={10}
-        navigation={true}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper2"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        onSwiper={setThumbsSwiper}
-        spaceBetween={10}
-        slidesPerView={4}
-        freeMode={true}
-        watchSlidesProgress={true}
-        modules={[FreeMode, Navigation, Thumbs]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img src="https://swiperjs.com/demos/images/nature-10.jpg" />
-        </SwiperSlide>
-      </Swiper> */}
-            </div>
+            <div className="md:w-[552px] md:h-[620px] border"></div>
 
             <div>
               <div className="flex gap-4 items-center">
@@ -162,12 +69,12 @@ const Bikroy = () => {
             </div>
           </div>
 
-          <div className="w-full static">
+          <div className="w-full sticky top-0">
             <div className="border  w-full">
               {" "}
               <h1 className="p-5">
                 For sale by
-                <span>Xâ Ñã</span>
+                <span> Xâ Ñã</span>
               </h1>
             </div>
 
@@ -181,8 +88,6 @@ const Bikroy = () => {
             </div>
             <div className="flex gap-4 border items-center p-5">
               <BsWechat className="bg-[#009877] w-8 h-8 text-white p-2 rounded-full" />
-
-              {/* <MdOutlinePhone  /> */}
 
               <div className="">
                 <h1>Chat</h1>
